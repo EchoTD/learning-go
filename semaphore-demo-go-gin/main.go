939f3aa9d67a132fce1b985956318845a07e1584
Can Gursoy <alaaddin.c.gursoy@gmail.com>
2025-03-19 16:07:39 +0300

    first checkpoint

---
+++ semaphore-demo-go-gin/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	// "net/http"
+)
+
+var router *gin.Engine
+
+func main() {
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", showIndexPage)
+
+	router.Run()
+
+}
